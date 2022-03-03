7c8e6b87b65f3f0c9f3bbd330406c7ab607972da
anastassiya.savostina <a.savostina@icloud.com>
2022-03-03 16:17:31 -0700

    done

--- server.js
+++ server.js
@@ -3,18 +3,23 @@ const express = require('express');
 //in place of the http variable that you would use for a node path
 const app = express();
 const path = require("path");
-
+const bodyParser = require("body-parser");
+const nodemailer = require("nodemailer");
 //setting up the express App 
 
 var PORT=8080;
 
 //Setting up the Express app to handle data parsing for sending POST requests
 //app.use = tells app to use the following info
-
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(__dirname+'/public'));
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 
+//Set the view engine as jade
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 //routes
 //Basic routes that send the user to the different html pages
 app.get("/", function (req,res){
@@ -28,18 +33,50 @@ app.get("/contact", function (req,res){
   res.sendFile(path.join(__dirname, "contact.html"))
 });
 
-app.get("/events", function (req,res){
-  res.sendFile(path.join(__dirname, "events.html"))
+app.get("/gallery", function (req,res){
+  res.sendFile(path.join(__dirname, "gallery.html"))
 });
 
-app.get("/review", function (req,res){
-    res.sendFile(path.join(__dirname, "review.html"))
-  });
+
 
 //starts the server to begin listening for requests
 app.listen(PORT, function(){
   console.log("App is listening: "  + PORT)
 });
+
+
+
+
+
+
+
+//A post request for NODEMAILER https://nodemailer.com/about/
+app.post('/contact/send', function (req, res) {
+    var transporter = nodemailer.createTransport({
+        service: 'Gmail',
+        auth: {
+            user: 'michaelmakanjuola69@gmail.com',
+            pass: ''
+        }
+    })
+    var mailOptions = {
+        from: 'Mak G <michaelmakanjuola69@gmail.com>',
+        to: 'michaelmakanjuola69@gmail.com',
+        subject: 'Website Submission',
+        text: 'You have a submission with the following details... Name: ' + req.body.name + 'Email: ' + req.body.email + 'Message: ' + req.body.message,
+        html: '<p>You have a submission with the following details...</p><ul><li>Name: ' + req.body.name + '</li><li>Email: ' + req.body.email + '</li><li>Message: ' + req.body.message + '</li></ul>'
+    }
+    transporter.sendMail(mailOptions, function (error, info) {
+        if (error) {
+            console.log(error)
+            res.redirect('/')
+        } else {
+            console.log('Message Sent: ' + info.response)
+            res.redirect('/')
+        }
+    })
+})
+
 
 
 
